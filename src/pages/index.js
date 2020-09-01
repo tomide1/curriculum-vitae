e3ee085ac8a5c191e6d9ebd73495c4cb226419f0
Tomide Fash <cleriocuzio15@yahoo.com>
2020-09-01 21:44:35 +0100

    develop chore: added work sans to li & a, and added a PDF download link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import Skill from "../components/Skill"
+import pdfCv from "../assets/pdf/cv-tomide.pdf"
 
 import { Mail } from '@styled-icons/heroicons-outline'
 import { Phone } from '@styled-icons/heroicons-outline'
@@ -10,17 +10,20 @@ import { LinkedinOutline } from "@styled-icons/evaicons-outline"
 
 export default function Home({ data }) {
   return (
-      <div className="sm:m-8">
-          <div className="max-w-4xl h-full border-8 border-gray-400 rounded-sm p-4 sm:p-8 m-auto">
+      <div>
+          <div className="max-w-4xl h-full p-4 sm:p-8 m-auto box-border">
+              <div className="flex flex-row justify-center mb-4 md:mb-8">
+                  <a className="border border-sm border-blue-500 px-4 py-2" href={pdfCv}>Download PDF</a>
+              </div>
               <div className="flex flex-col md:flex-row justify-start">
-                  <div className="sm:w-full md:w-1/2 mx-1 md:mx-2 my-2 p-2 text-center box-border">
+                  <div className="sm:w-full md:w-1/2 mx-1 md:mx-2 p-2 text-center box-border">
                       <div>
                           <h1 className="font-bold text-xl tracking-widest">TOMIDE FASUGBA</h1>
                           <p className="italic text-gray-600 mb-4">Software Engineer</p>
                           <Img className="rounded-full" fixed={data.file.childImageSharp.fixed}/>
                       </div>
                   </div>
-                  <div className="sm:w-full md:w-1/2 mx-1 md:mx-2 my-2 p-2 text-center box-border">
+                  <div className="sm:w-full md:w-1/2 mx-1 md:mx-2 mb-2 p-2 text-center box-border">
                       <div>
                           <h2 className="text-left tracking-wider border-t-2 border-b-2 py-2">CONTACT</h2>
                           <div className="border-b-2 py-1 flex justify-between">
@@ -49,7 +52,7 @@ export default function Home({ data }) {
                           <h3 className="text-left tracking-wide">University of Manchester</h3>
                           <p className="italic text-left text-gray-600">B.Sc.(Hons) Computer Science with Industrial Experience (Second Class, Division One) 2018</p>
                           <li className="text-left tracking-wider font-medium">Relevant coursework</li>
-                          <p className="text-left pyb-2">Java Object Oriented Programming, Fundamentals of Artificial Intelligence, Algorithms and Imperative Programming, Operating Systems, Software Engineering, Cryptography, User Experience, AI and Games, System Architecture, User Experience, Internet Of Things</p>
+                          <p className="text-left">Java Object Oriented Programming, Fundamentals of Artificial Intelligence, Algorithms and Imperative Programming, Operating Systems, Software Engineering, Cryptography, User Experience, AI and Games, Internet Of Things</p>
                           <li className="text-left tracking-wider">Final Year Project</li>
                           <p className="text-left">Ro-show: Built a Spring boot app for visualising research objects and designed a system for validating them through inspection of an rdf manifest using Shacl (shapes constraint language)</p>
                           <li className="text-left tracking-wider">Other Projects</li>
@@ -60,9 +63,8 @@ export default function Home({ data }) {
                   <div className="my-2 p-2 text-left box-border">
                       <h2 className="text-left tracking-wider border-t-2 border-b-2 py-2 mb-2">EXPERIENCE</h2>
                       <div className="mb-4">
-                          <h3 className="tracking-wide text-left">Software Engineer</h3>
-                          <p className="italic text-left text-gray-600">The Hut Group</p>
-                          <p className="text-left">Mar. 2020 - Present</p>
+                          <h3 className="tracking-wide text-left">The Hut Group</h3>
+                          <p className="italic text-left text-gray-600">Software Engineer (Mar. 2020 - Present)</p>
                           <ul className="mt-2 list-disc list-inside text-left">
                               <li>Currently in charge of front-end firefighters; an initiative that handles urgent customer-facing issues that affect a web hosting platform comprising 100+ websites</li>
                               <li>Engineered CSS sharding across 100+ sites, reducing the css loaded in the browser to improve site performance in page load times, and baking accessibility into them</li>
@@ -73,9 +75,7 @@ export default function Home({ data }) {
                       </div>
 
                       <div className="mb-4">
-                          <h3 className="tracking-wide text-left">Graduate Software Engineer</h3>
-                          <p className="italic text-left text-gray-600">The Hut Group</p>
-                          <p className="text-left">Aug. 2018 - Mar. 2020</p>
+                          <p className="italic text-left text-gray-600">Graduate Software Engineer (Aug. 2018 - Mar. 2020)</p>
                           <ul className="mt-2 list-disc list-inside text-left">
                               <li>First line support, bug investigation, documentation and fixes</li>
                               <li>Led performance improvement of a monolith application, purging deprecated/redundant libraries, remove legacy code, made websites using separate mobile and desktop pages responsive (hence cutting page sizes by almost 50%)</li>
@@ -83,17 +83,16 @@ export default function Home({ data }) {
                           </ul>
                       </div>
                       <div className="mb-4">
-                          <h3 className="tracking-wide text-left">Intern Software Engineer</h3>
-                          <p className="italic text-left text-gray-600">Mentor Graphics; a Siemens Company</p>
-                          <p className="text-left">Jul. 2016 - Jun. 2017</p>
+                          <h3 className="tracking-wide text-left">Mentor Graphics</h3>
+                          <p className="italic text-left text-gray-600">Intern Software Engineer (Jul. 2016 - Jun. 2017)</p>
                           <ul className="mt-2 list-disc list-inside text-left">
                               <li>Bug investigation, documentation, and fixes</li>
                               <li>Developed UI components for a Wire-framing desktop app in Java using Swing</li>
                           </ul>
                       </div>
                       <div>
-                          <h3 className="tracking-wide text-left">Engineering Intern</h3>
-                          <p className="italic text-left text-gray-600">ATG Access Limited</p>
+                          <h3 className="tracking-wide text-left">ATG Access Limited</h3>
+                          <p className="italic text-left text-gray-600">Engineering Intern</p>
                           <p className="text-left">Aug. 2015 - Sept. 2015</p>
                           <ul className="mt-2 list-disc list-inside text-left">
                               <li>Occasionally partook in bollard installation on-site</li>
@@ -104,7 +103,7 @@ export default function Home({ data }) {
                   </div>
                   <div className="my-2 p-2 text-left box-border">
                       <h2 className="text-left tracking-wider border-t-2 border-b-2 py-2 mb-2">PROJECTS</h2>
-                      <div className="mb-4">
+                      <div>
                           <h3 className="text-left tracking-wide">MY CV</h3>
                           <p className="text-left pyb-2">Built out my cv using React Gatsby for performance and scalability, graphQL, tailwind CSS, purgeCSS for cutting out unused css code (introduced by tailwind CSS) from the production build, and puppeteer for generating PDFs</p>
                       </div>
@@ -112,22 +111,12 @@ export default function Home({ data }) {
                   <div className="my-2 p-2 text-left box-border">
                       <h2 className="text-left tracking-wider border-t-2 border-b-2 py-2 mb-2">SKILLS</h2>
                       <div className="text-left">
-                          <Skill skillName={"Object-oriented programming"}/>
-                          <Skill skillName={"Full stack web development"}/>
-                          <Skill skillName={"Teaching"}/>
-                          <Skill skillName={"Java"}/>
-                          <Skill skillName={"Javascript"}/>
-                          <Skill skillName={"Sass"}/>
-                          <Skill skillName={"HTML"}/>
-                          <Skill skillName={"SQL"}/>
-                          <Skill skillName={"Python"}/>
-                          <Skill skillName={"Docker"}/>
-                          <Skill skillName={"React"}/>
-                          <Skill skillName={"Project management"}/>
-                          <Skill skillName={"Mentoring"}/>
-                          <Skill skillName={"Haskell"}/>
-                          <Skill skillName={"SCRUM"}/>
-                          <Skill skillName={"Customer Service"}/>
+                          <h3 className="tracking-wide text-left">Technical skills</h3>
+                          <p className="text-left pb-2">
+                              Object-oriented programming, full stack web development, Java, Javascript, Sass, HTML, SQL, Python, Docker, React, Haskell, SCRUM</p>
+                          <h3 className="tracking-wide text-left">Professional skills</h3>
+
+                          <p className="">Teaching, project management, mentoring, customer Service</p>
                       </div>
                   </div>
               </div>
